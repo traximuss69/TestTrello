987703aed70983a6dcce3ad7446ce6db9c22daea
Timur Latypov <timurlatypov209@gmail.com>
2025-04-09 19:46:47 +0500

    малой повзрослел

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(".!.")
+	fmt.Println("  ()")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println("  ||")
+	fmt.Println(" ()()")
 }
