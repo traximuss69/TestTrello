25734a5339357627c4e7fea0354390f12be316c7
Timur Latypov <timurlatypov209@gmail.com>
2025-04-09 20:14:24 +0500

    Сделал основу

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Выберите действие:")
+	fmt.Println("1. Создать Доску")
+	fmt.Println("2. Посмотреть все доски")
+	fmt.Println("3. Управлять доской")
+	fmt.Println("4. Загрузить данные из файла")
+	fmt.Println("5. Сохранить данные в файл")
+	fmt.Println("0. Выход")
 }
