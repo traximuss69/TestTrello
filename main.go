b03226a9b1ca4792af67668974edb22710b6310a
Timur Latypov <timurlatypov209@gmail.com>
2025-04-09 22:31:29 +0500

    Сделал скелет

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Card struct {
+	ID          int
+	Title       string
+	Description string
+	Status      string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type List struct {
+	ID    int
+	Title string
+	Cards []Card
+}
+type Board struct {
+	ID    int
+	Title string
+	Lists []List
+}
 
 func main() {
-	fmt.Println("Выберите действие:")
-	fmt.Println("1. Создать Доску")
-	fmt.Println("2. Посмотреть все доски")
-	fmt.Println("3. Управлять доской")
-	fmt.Println("4. Загрузить данные из файла")
-	fmt.Println("5. Сохранить данные в файл")
-	fmt.Println("0. Выход")
+	var board []Board
+	var boardID int
+	var write int
+	for {
+		fmt.Println("Выберите действие:")
+		fmt.Println("1. Создать Доску")
+		fmt.Println("2. Посмотреть все доски")
+		fmt.Println("3. Управлять доской")
+		fmt.Println("4. Загрузить данные из файла")
+		fmt.Println("5. Сохранить данные в файл")
+		fmt.Println("0. Выход")
+		fmt.Print("Введите число:")
+		fmt.Scan(&write)
+	}
 }
